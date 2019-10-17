a6127a59735727e72fe7e08c4c9808382c48a21d
Pr0xi <hawk2106@mts.rs>
2019-10-17 23:40:24 +0200

    loops excersize

--- Bit_PP/loops.js
+++ Bit_PP/loops.js
@@ -1,8 +1,94 @@
-var sum = 0;
-var pass = 1;
+// var sum = 0;
+// var pass = 1;
 
-while (pass <= 100) {
-    sum = sum + pass;
-    console.log(sum);
-    pass++
+// while (pass <= 100) {
+//     sum = sum + pass;
+//     console.log(sum);
+//     pass++
+// }
+
+
+// var pass1=0;
+
+// while (pass1<100) {
+//     console.log("BIT")
+//     pass1++
+// }
+
+
+// var out="";
+
+// while (out.length<4) {
+//     console.log(out=out+"*")
+//     out
+// }
+
+// var sum1=0;
+// var ind=1;
+
+// while (ind<=100) {
+//     console.log(val=ind*2)
+// sum1+=val
+// ind++
+        
+// }
+
+// var sum2=0;
+// var number=2;
+// while (number<=100) {
+//     console.log (sum2+=number)
+//     number+=2
+// }
+
+
+
+// for (var x=0; x<=10; x++) {
+//     console.log (x*x)
+// }
+
+
+
+// for (var y=0; y<=15; y++) {
+//     if(y%2==0) 
+//         console.log ("odd")
+    
+//     else (y%2!==0) 
+//         console.log ("even")
+    
+//     ovde je endless loop!
+// }
+
+
+var number = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       number += x;
+    }
 }
+console.log(number);
+
+
+
+
+var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+
+for (var y=0; y<x.length; y++) {
+console.log (x[y])
+}
+
+
+
+
+var a = [
+    [1, 2, 1, 24], 
+    [8, 11, 9, 4], 
+    [7, 0, 7, 27]
+    ];
+    var p=[]
+
+    for (var z=0; z<a.length; z++) {
+         console.log (a[z]+p)
+    }
+    
