26623b3c8d7b4df2d77b32c4088c4bcb24e76314
Pr0xi <hawk2106@mts.rs>
2019-11-14 06:23:13 +0100

    work1

--- jquery/animated.js
+++ jquery/animated.js
@@ -1,24 +1,55 @@
 $(function () {
-    var subs = ['./images/Kilo-Class_Russian_Submarine_MOD_45165129_0.jpg', './images/RTX3CFVP_0.jpg']
-    var copters = ["./images/409572ae874bc9273e9a4b21b62badce.jpeg", "./images/BC17E7DB-AB7E-48FD-8E37-C8834344984F_cx0_cy7_cw0_w1023_r1_s.jpg", "./images/mi-171a2_kbo17_3.jpg"]
-    var body = $('body')
-    var div1 = $("<div>")
-    var div2 = $("<div>")
-    // body.append(div1)
-    // slika = $("<img>").attr("src", subs[1])
-    // div1.append(slika, slika)
+    var link1 = "./images/sub1.jpg";
+    var link2 = "./images/sub2.jpg";
+    var link3 = "./images/sub3.jpg";
+    var link4 = "./images/copter1.jpg";
+    var link5 = "./images/copter2.jpg";
+    var link6 = "./images/copter3.jpg";
+    var subs = [link1, link2, link3, link4, link5, link6];
+    var $div = $('div');
+
+
 
     for (var i = 0; i < subs.length; i++) {
-        var slika = $("<img>").attr("src", subs[i])
-        div1.append(slika)
-        $('<img>').width('300px');
-    }
-    body.append(div1)
+   var randomWidth = Math.floor(Math.random()*300) + 100;
 
-    for (var i = 0; i < copters.length; i++) {
-        var slika1 = $("<img>").attr("src", copters[i])
-        div2.append(slika1)
-    }
-    body.append(div2)
+   var $image = $('<img>')
+   .attr("src", subs[i])
+   .width(randomWidth)
 
-});
+   $div.append($image)
+    }
+    $('body').append($div);
+    var addingBorder = true;
+    $('img').each(function (i, image) {
+
+        if (addingBorder && w<300) {
+        image.style.border = '10px solid black'
+        
+    }
+        else {
+            addingBorder = false;
+        }
+}()
+
+
+
+    // var body = $('body')
+    // var div1 = $("<div>")
+    // var div2 = $("<div>")
+    // // body.append(div1)
+    // // slika = $("<img>").attr("src", subs[1])
+    // // div1.append(slika, slika)
+
+    // for (var i = 0; i < subs.length; i++) {
+    //     var slika = $("<img>").attr("src", subs[i])
+    //     div1.append(slika)
+    //     $('<img>').width('300px');
+    // }
+    // body.append(div1)
+
+    // for (var i = 0; i < copters.length; i++) {
+    //     var slika1 = $("<img>").attr("src", copters[i])
+    //     div2.append(slika1)
+    // }
+    // body.append(div2))
